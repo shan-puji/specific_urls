be1e632662421f07d25ee97250f9080cc5f6ed56
shan-puji <pujitha.kuthada3@gmail.com>
2023-03-16 14:04:15 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Avinash(request):
+    return HttpResponse('<h1>My brother is always funny</h1>')
